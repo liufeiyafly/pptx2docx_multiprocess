1974da2853fb052c3f07985f9f71e2e71b311aa2
liufeiyafly <liufeiyafly@163.com>
2020-02-25 18:35:37 +0800

    多进程版本

--- pptx2docx_multip.py
+++ pptx2docx_multip.py
@@ -4,7 +4,7 @@ from docx import Document
 from docx.oxml.ns import qn
 import time
 
-from multiprocessing import Pool#,Process
+from multiprocessing import Pool
 import os
 
 def Group_digui(group, l=[]):  # 递归寻找group组合对象里面的文字，（group里面可能套了group）
@@ -14,7 +14,6 @@ def Group_digui(group, l=[]):  # 递归寻找group组合对象里面的文字，
     for i in group.shapes:
         if str(type(i)) == auto_object or str(type(i)) == place_object:
             s2 = i.text.encode('gbk', 'ignore').decode('gbk', 'ignore')
-            # print(s2)
             l.append(s2.strip().replace('\x0b', ' '))
         elif i.shape_type == 6:  # 等于6即 是group组合
             Group_digui(i)  # 递归访问即可
@@ -25,28 +24,20 @@ def ppt2pptx(filename):
     doc.styles['Normal'].font.name = u'宋体'
     doc.styles['Normal']._element.rPr.rFonts.set(qn('w:eastAsia'), u'宋体')
 
-    # filename=r'C:\Users\liufe\Desktop\1.pptx'
     prs=Presentation(filename)
-
     num=len(prs.slides) # 幻灯片的页数
-    # print(num)
     auto_object='<class \'pptx.shapes.autoshape.Shape\'>'
     place_object='<class \'pptx.shapes.placeholder.SlidePlaceholder\'>'
     group_object='<class \'pptx.shapes.group.GroupShape\'>'
     L=[]
 
     for i in range(num):
-        # print(i)
         slide = prs.slides[i]  # 通过索引序号 访问每页幻灯片
-        # print(slide)
         body_shapes = slide.shapes  # slide.shapes 就是每页幻灯片中的所有元素。
-        # if len(body_shapes)==0:
-        #     return
+
         for j in body_shapes:
             if str(type(j)) == auto_object or str(type(j)) == place_object:  # 图形或者文本框是有 .text属性的，可以直接访问
-                # print(j.text.encode('gbk','ignore').decode('gbk','ignore'))
-                s1 = j.text.encode('gbk', 'ignore').decode('gbk', 'ignore')  # 我靠，用这个代码解决不能打印和写入文件'gbk'特殊编码的问题了！！！
-                # print(s1)
+                s1 = j.text.encode('gbk', 'ignore').decode('gbk', 'ignore')  # 用这个代码解决不能打印和写入文件'gbk'特殊编码的问题了！！！
                 L.append(s1.strip().replace('\x0b', ' '))
 
             elif str(type(j)) == group_object:
@@ -62,7 +53,6 @@ def ppt2pptx(filename):
     s = ' '.join(L)  # 以空格分开各个内容，也可以用回车
     doc.add_paragraph(s)
     doc.save('{}.docx'.format(filename[:-5]))  # 生成的docx保存到pptx所在文件夹
-
 
 if __name__ == "__main__":
     path=r'D:\test' #选择的路径，会处理路径下所有的*.pptx
